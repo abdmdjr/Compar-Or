a4dba36241b371f0307a57a8e7e7ad071f4c2ab1
Abdel <medjaherirany@gmail.com>
2020-10-07 19:01:47 +0200

    update index.js

--- server/index.js
+++ server/index.js
@@ -1,9 +1,13 @@
 const { Nuxt, Builder } = require('nuxt')
 const express = require('express')
+const mongoose = require('mongoose')
 require('dotenv').config()
 const consola = require('consola')
 const config = require('../nuxt.config.js')
 const coinRouter = require('./routes/coinRoutes')
+const { goldAvenue } = require('./controllers/updateGoldAvenue.js')
+const { lingor } = require('./controllers/updateLingor.js')
+const { auCoffre } = require('./controllers/updateAuCoffre.js')
 const app = express()
 
 config.dev = process.env.NODE_ENV !== 'production'
@@ -19,11 +23,26 @@ async function start() {
 		await builder.build()
 	}
 
-	app.listen(port, host)
-	consola.ready({
-		message: `Server listening on http://${host}:${port}`,
-		badge: true
-	})
+	mongoose
+		.connect(process.env.DB_URL, {
+			useNewUrlParser: true,
+			useUnifiedTopology: true,
+			useFindAndModify: false
+		})
+		.then(() =>
+			consola.ready({ message: 'Connected to database', badge: true })
+		)
+		.then(() => {
+			app.listen(port, host)
+			consola.ready({
+				message: `Server listening on http://${host}:${port}`,
+				badge: true
+			})
+		})
+
+	goldAvenue()
+	lingor()
+	auCoffre()
 
 	app.use(express.json())
 	app.use('/api', coinRouter)
