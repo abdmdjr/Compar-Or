d20e98ab949bb52bb40724342e7fb1150fe20a3a
Abdel <medjaherirany@gmail.com>
2021-01-03 15:50:05 +0100

    feat(Auth): backend routes + JWT => /register, /login

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,5 +1,7 @@
 const express = require('express')
-const { registerValidation } = require('../validation')
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const { registerValidation, loginValidation } = require('../validation')
 const User = require('../models/userModel')
 const app = express()
 
@@ -10,17 +12,36 @@ app.post('/register', async (req, res) => {
 	const emailExist = await User.findOne({ email: req.body.email })
 	if (emailExist) return res.status(400).send('Email already exists')
 
+	const salt = await bcrypt.genSalt(10)
+	const hashedPassword = await bcrypt.hash(req.body.password, salt)
+
 	const user = new User({
 		username: req.body.username,
 		email: req.body.email,
-		password: req.body.password
+		password: hashedPassword
 	})
 	try {
-		const savedUser = await user.save()
-		res.send(savedUser)
+		await user.save()
+		res.send({ user: user._id })
 	} catch (err) {
 		res.status(400).send(err)
 	}
+})
+
+app.post('/login', async (req, res) => {
+	const { error } = loginValidation(req.body)
+	if (error) return res.status(400).send(error.details[0].message)
+
+	const user = await User.findOne({ email: req.body.email })
+	if (!user) return res.status(400).send('Email or password is wrong')
+
+	const validPassword = await bcrypt.compare(req.body.password, user.password)
+	if (!validPassword) return res.status(400).send('Invalid password')
+
+	const token = jwt.sign({ _id: user._id }, process.env.TOKEN_SECRET)
+	res.header('auth-token', token)
+
+	res.send('Success')
 })
 
 module.exports = app
